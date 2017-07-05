fc87c9a02e58a9c4d837f34e775751bf2b833e55
ibrown <ibrown@twitter.com>
2017-07-05 09:27:45 -0700

    sort of working

--- src/js/voronoi.js
+++ src/js/voronoi.js
@@ -1,19 +1,38 @@
-const http = require('http'); // Loads the http module
-var d3 = require("d3");
+var fs = require('fs');
+var d3 = require('d3');
+var jsdom = require('node-jsdom');
+var cheerio = require("cheerio");
+
+    var document = jsdom.jsdom();
+    var body = d3.select(document);
 
 
 
-http.createServer((request, response) => {
 
-	// 1. Tell the browser everything is OK (Status code 200), and the data is in plain text
-	response.writeHead(200, {
-		'Content-Type': 'text/plain'
-	    });
 
-	// 2. Write the announced text to the body of the page
-	response.write('Hello, World!\n');
 
-	// 3. Tell the server that all of the response headers and body have been sent
-	response.end();
+var data = [3, 5, 8, 4, 7];
 
-    }).listen(1337); // 4. Tells the server what port to be on
+// create the outer svg
+var svg = d3.select(document.body).append('svg')
+    .attr('height', 100)
+    .attr('width', 500);
+
+// append circles for each data point sized relative to the value
+svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', function (d, i) {
+            return (i + 1) * 100 - 50;
+        })
+    .attr('cy', svg.attr('height') / 2)
+    .attr('r', function (d) {
+            return d * 5;
+        });
+
+
+
+var x  = cheerio.load('<html><body><h2 class = "title">Hello world</h2></body></html>');
+console.log(svg.node().outerHTML);  
+
